68628c22e234bf0175649a1837d93572e76a09dd
Rayan <rayansiqueira@gmail.com>
2023-09-28 14:41:01 -0300

    start my portfolio

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Footer from "./components/footer";
+import Home from "./components/home";
+
+export default function App() {  
+
+  return (
+    <>
+      <main className="w-full h-full flex flex-col">
+        <Home />
+      </main>
+      <Footer />
+    </>
+  )
+}
+
