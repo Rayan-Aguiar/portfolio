6bf719da3e7ac4b521631994df2cb2d70980b63a
rayan-aguiar <rayansiqueira@gmail.com>
2025-03-10 17:32:04 -0300

    feat: adicionar destaque ao link ativo no menu lateral

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -6,13 +6,15 @@ import {
   HomeIcon,
   Laptop2,
 } from "lucide-react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function AsideContent() {
+  const location = useLocation();
+  const isActive = (path: string) => location.pathname === path ? 'bg-lightblue/20' : 'hover:bg-white/10'
   return (
     <>
       <Link to="/">
-        <span className="flex gap-2 items-center hover:bg-white/10 p-2 font-bold w-full rounded-md duration-75">
+        <span className={`flex gap-2 items-center p-2 font-bold w-full rounded-md duration-75 ${isActive("/")}`}>
           <HomeIcon className="text-lightblue w-4 h-4" /> Início
         </span>
       </Link>
@@ -21,19 +23,19 @@ export default function AsideContent() {
         <span className="font-semibold text-zinc-500 text-sm">Eu</span>
         <ul className="mt-2">
           <Link to="/aboutme">
-            <li className="flex gap-2 items-center hover:bg-white/10 p-2 w-full rounded-md duration-75 cursor-pointer text-sm">
+            <li className={`flex gap-2 items-center p-2 w-full rounded-md duration-75 cursor-pointer text-sm ${isActive("/aboutme")}`}>
               <BookOpen className="text-lightblue w-4 h-4" />
               Sobre mim
             </li>
           </Link>
           <Link to="/projects">
-            <li className="flex gap-2 items-center hover:bg-white/10 p-2 w-full rounded-md duration-75 cursor-pointer text-sm">
+            <li className={`flex gap-2 items-center p-2 w-full rounded-md duration-75 cursor-pointer text-sm ${isActive("/projects")}`}>
               <FolderOpenDot className="text-lightblue w-4 h-4" />
               Projetos
             </li>
           </Link>
           <Link to="/technologies">
-            <li className="flex gap-2 items-center hover:bg-white/10 p-2 w-full rounded-md duration-75 cursor-pointer text-sm">
+            <li className={`flex gap-2 items-center p-2 w-full rounded-md duration-75 cursor-pointer text-sm ${isActive("/technologies")}`}>
               <Laptop2 className="text-lightblue w-4 h-4" />
               Tecnologias e Ferramentas
             </li>
