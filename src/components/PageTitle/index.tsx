c0315b6595c16f7d38399be8bea0362831c2b45e
rayan-aguiar <rayansiqueira@gmail.com>
2025-03-24 20:42:00 -0300

    feat: adicionar componente PageTitle para atualização dinâmica do título da página

---
+++ src/components/PageTitle/index.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+
+const pageTitle: {[key: string]: string} = {
+    '/': 'Inicio - Meu Portfolio',
+    "/aboutme": "Sobre Mim - Meu Portfólio",
+    "/projects": "Projetos - Meu Portfólio",
+    "/technologies": "Tecnologias - Meu Portfólio",
+}
+export default function PageTitle() {
+    const location = useLocation()
+
+    useEffect(() => {
+        document.title = pageTitle[location.pathname] || 'Meu Portfolio'
+    }, [location.pathname])
+
+    return null;
+}
