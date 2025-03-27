20cafb017ca57f8a40e99c61e67a2eeeeb9e8f04
rayan-aguiar <rayansiqueira@gmail.com>
2025-03-27 13:21:19 -0300

    fix: corrigir título da página para exibir "Página Não Encontrada" quando a rota não é reconhecida

--- src/components/PageTitle/index.tsx
+++ src/components/PageTitle/index.tsx
@@ -1,19 +1,20 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
+const pageTitle: { [key: string]: string } = {
+  "/": "Inicio - Meu Portfolio",
+  "/aboutme": "Sobre Mim - Meu Portfólio",
+  "/projects": "Projetos - Meu Portfólio",
+  "/technologies": "Tecnologias - Meu Portfólio",
+  "*": "Página Não Encontrada - Meu Portfólio", 
+};
 
-const pageTitle: {[key: string]: string} = {
-    '/': 'Inicio - Meu Portfolio',
-    "/aboutme": "Sobre Mim - Meu Portfólio",
-    "/projects": "Projetos - Meu Portfólio",
-    "/technologies": "Tecnologias - Meu Portfólio",
-}
 export default function PageTitle() {
-    const location = useLocation()
+  const location = useLocation();
 
-    useEffect(() => {
-        document.title = pageTitle[location.pathname] || 'Meu Portfolio'
-    }, [location.pathname])
+  useEffect(() => {
+    document.title = pageTitle[location.pathname] || pageTitle["*"];
+  }, [location.pathname]);
 
-    return null;
+  return null;
 }
